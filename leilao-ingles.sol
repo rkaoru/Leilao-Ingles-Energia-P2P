b853aaf4ad47977a56edcd6ce2e69620cd4f991e
Ricardo Kaoru Takahashi Vizentin <rkaoru@weg.net>
2024-06-17 16:28:34 -0300

    contrato atualizado

--- leilao-ingles.sol
+++ leilao-ingles.sol
@@ -20,6 +20,7 @@ contract MercadoEnergiaLeilao {
     bool public leilaoAtivo;
     Lance public maiorLance;
     mapping(address => uint) public lances;
+    address[] public participantes;
 
     event NovaOferta(address produtor, uint quantidade, uint precoMinimoPorKwh);
     event NovoLance(address comprador, uint valor);
@@ -31,8 +32,8 @@ contract MercadoEnergiaLeilao {
         _;
     }
 
-    modifier apenasDepoisDoFinal() {
-        require(block.timestamp >= tempoFinal, "Leilao ainda nao terminou");
+    modifier apenasBeneficiario() {
+        require(msg.sender == beneficiario, "Apenas o beneficiario pode finalizar o leilao");
         _;
     }
 
@@ -42,10 +43,29 @@ contract MercadoEnergiaLeilao {
         tempoFinal = block.timestamp + (_duracaoLeilaoEmMinutos * 1 minutes);
         beneficiario = _beneficiario;
         leilaoAtivo = true;
+    }
 
+    function tempoFinalLeilao() public view returns (uint) {
+        return tempoFinal;
+    }
+
+    function definirOferta(address payable _produtor, uint _quantidadeDisponivel, uint _precoMinimoPorKwhEmReais) public {
+        oferta = Oferta({
+            produtor: _produtor,
+            quantidadeDisponivel: _quantidadeDisponivel,
+            precoMinimoPorKwhEmReais: _precoMinimoPorKwhEmReais,
+            ativa: true
+        });
+
+        emit NovaOferta(_produtor, _quantidadeDisponivel, _precoMinimoPorKwhEmReais);
     }
 
     function fazerLance() public payable apenasAntesDoFinal {
+        if (block.timestamp >= tempoFinal) {
+            finalizarLeilaoAutomaticamente();
+            return;
+        }
+
         require(msg.value > maiorLance.valor, "Lance deve ser maior que o atual maior lance");
         require(msg.value >= oferta.precoMinimoPorKwhEmReais * 1 ether / etherToReais, "Lance abaixo do preco minimo");
 
@@ -53,20 +73,61 @@ contract MercadoEnergiaLeilao {
             lances[maiorLance.comprador] += maiorLance.valor;
         }
 
+        if (lances[msg.sender] == 0) {
+            participantes.push(msg.sender);
+        }
+
         maiorLance = Lance(payable(msg.sender), msg.value);
 
         emit NovoLance(msg.sender, msg.value);
     }
 
-    function finalizarLeilao() public apenasDepoisDoFinal {
+    function finalizarLeilao() public apenasBeneficiario {
         require(leilaoAtivo, "Leilao ja foi finalizado");
         leilaoAtivo = false;
 
+        // Transferir valor do maior lance para o beneficiario
         if (maiorLance.comprador != address(0)) {
             beneficiario.transfer(maiorLance.valor);
             emit LeilaoFinalizado(maiorLance.comprador, maiorLance.valor);
         } else {
             emit LeilaoFinalizado(address(0), 0);
+        }
+
+        // Restituir lances para todos os participantes exceto o vencedor
+        for (uint i = 0; i < participantes.length; i++) {
+            address addr = participantes[i];
+            uint valor = lances[addr];
+            if (addr != maiorLance.comprador && valor > 0) {
+                lances[addr] = 0;
+                payable(addr).transfer(valor);
+                emit LancesRestituidos(addr, valor);
+            }
+        }
+    }
+
+    function finalizarLeilaoAutomaticamente() private {
+        require(block.timestamp >= tempoFinal, "Leilao ainda nao terminou");
+
+        leilaoAtivo = false;
+
+        // Transferir valor do maior lance para o beneficiario
+        if (maiorLance.comprador != address(0)) {
+            beneficiario.transfer(maiorLance.valor);
+            emit LeilaoFinalizado(maiorLance.comprador, maiorLance.valor);
+        } else {
+            emit LeilaoFinalizado(address(0), 0);
+        }
+
+        // Restituir lances para todos os participantes exceto o vencedor
+        for (uint i = 0; i < participantes.length; i++) {
+            address addr = participantes[i];
+            uint valor = lances[addr];
+            if (addr != maiorLance.comprador && valor > 0) {
+                lances[addr] = 0;
+                payable(addr).transfer(valor);
+                emit LancesRestituidos(addr, valor);
+            }
         }
     }
 
